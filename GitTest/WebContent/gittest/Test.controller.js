7b2237b25cdaa8932431ead8d80b804ddd0d4703
ratish197 <ratish197@users.noreply.github.com>
2017-04-12 15:15:28 +0530

    Update Test.controller.js

--- GitTest/WebContent/gittest/Test.controller.js
+++ GitTest/WebContent/gittest/Test.controller.js
@@ -5,9 +5,9 @@ sap.ui.controller("gittest.Test", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf gittest.Test
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
